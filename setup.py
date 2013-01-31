48816f5ff374980c323b3acdff037c75b8578bb3
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2013-01-31 16:02:36 +0100

    prepare for sdist

--- setup.py
+++ setup.py
@@ -11,5 +11,17 @@ setup(
     author = 'Sebastian Heimann',
     author_email = 'sebastian.heimann@gfz-potsdam.de',
     url = 'http://emolch.github.com/guts/',
+    keywords = [ 'data-binding', 'xml', 'yaml' ],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+
 )
 
